fe5850ede992dea0e9a199322d369e6fda48a5e1
Esther Ng <esther.hc.ng@gmail.com>
2016-10-04 14:56:51 -0700

    Added markets and vendors resources/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :markets, except: [:destroy] do
+    resources :vendors
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
