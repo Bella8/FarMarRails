fdc2e67b389c2e3938e60ebcff2739f4d66dd810
Beylul <bkbreab8@gmail.com>
2016-10-04 16:08:14 -0700

    Generated models with relationships.

---
+++ db/migrate/20161004220007_create_vendors.rb
@@ -0,0 +1,10 @@
+class CreateVendors < ActiveRecord::Migration
+  def change
+    create_table :vendors do |t|
+      t.belongs_to :market, index: true
+      t.string :name
+      t.integer :number_of_employees
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20161004220021_create_sales.rb
@@ -0,0 +1,11 @@
+class CreateSales < ActiveRecord::Migration
+  def change
+    create_table :sales do |t|
+      t.belongs_to :product, index: true
+      t.integer :amount
+      t.datetime :purchase_time
+
+      t.timestamps null: false
+    end
+  end
+end
