a7c30c97c71af9cff7dc6dc2cefbbf5c6028d1f4
Beylul <bkbreab8@gmail.com>
2016-10-05 14:51:36 -0700

    added new and create in the controller and new view

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -8,11 +8,28 @@ end
     @link = "market"
   end
 
+  def new
+  @market = Market.new
+  end
+
+  def create
+      @new_market_create = Market.new(market_params) #task_params is a method
+      if @new_market_create.save
+        redirect_to markets_path
+      else
+        render :new
+      end
+    end
+
+    
+
   def user_all
     @markets = Market.all
     @link = "user"
     render "index"
   end
+
+
 
   def user_show
     id = params[:id]
@@ -21,4 +38,9 @@ end
     @link= "market"
     render "show"
   end
+
+  private
+def  market_params
+  params.require(:market).permit(:name, :address, :city, :county, :state, :zip)
+end
 end
