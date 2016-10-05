39ddaa11bcbf7e083d27e4b66ecc1467b6c98154
Esther Ng <esther.hc.ng@gmail.com>
2016-10-05 14:34:51 -0700

    Completed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,37 @@
 
 require 'csv'
 
+CSV.read('./seed_csvs/markets.csv').each do |line|
+  Market.create(
+    id: line[0].to_i,
+    name: line[1],
+    address: line[2],
+    city: line[3],
+    county: line[4],
+    state: line[5],
+    zip: line[6])
+    # info << Market.new(id, name, address, city, county, state, zip)
+end
 
-    CSV.read('./seed_csvs/markets.csv').each do |line|
-      Market.create(
-        id: line[0].to_i,
-        name: line[1],
-        address: line[2],
-        city: line[3],
-        county: line[4],
-        state: line[5],
-        zip: line[6])
-        # info << Market.new(id, name, address, city, county, state, zip)
-        end
+CSV.read('./seed_csvs/vendors.csv').each do |line|
+  Vendor.create(
+  id: line[0].to_i,
+  name: line[1],
+  number_of_employees: line[2].to_i,
+  market_id: line[3].to_i)
+end
+
+CSV.read('./seed_csvs/products.csv').each do |line|
+  Product.create(
+    id: line[0].to_i,
+    name: line[1],
+    vendor_id: line[2].to_i)
+end
+
+CSV.read('./seed_csvs/sales.csv').each do |line|
+  Sale.create(
+    id: line[0].to_i,
+    amount: line[1].to_i,
+    purchase_time: DateTime.parse(line[2]),
+    product_id: line[4].to_i)
+end
