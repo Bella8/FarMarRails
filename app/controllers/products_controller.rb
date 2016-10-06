e79854675ef0e4b715a191cf1f82104fc4c8d71c
Esther Ng <esther.hc.ng@gmail.com>
2016-10-06 16:41:13 -0700

    functioning edit product link

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,40 @@ class ProductsController < ApplicationController
     redirect_to vendor_path(vendor_id)
   end
 
+  def show
+    id = params[:id]
+    @vendor_product = Product.find(id)
+  end
+
+  def edit
+    id = params[:id]
+    @vendor_product = Product.find(id)
+=begin
+  @vendor = Vendor.find(params[:vendor_id])
+  @product = Product.find(params[:id])
+=end
+
+  end
+
+  def update
+    @vendor_product = Product.find(params[:id])
+    if @vendor_product.update(vendor_product_params)
+      redirect_to vendor_path(params[:vendor_id])
+    else
+      render :edit
+    end
+=begin
+    @product = Product.find(params[:id])
+    @vendor = Vendor.find(params[:vendor_id])
+    @product.vendor_id = @vendor.id
+    if @vendor_product.update(vendor_product_params)
+      redirect_to vendor_path(params[@vendor.id])
+    else
+      render :edit
+    end
+=end
+  end
+
   private
 
   def  vendor_product_params
