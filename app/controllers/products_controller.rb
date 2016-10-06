d2801d08d17436dca010d58d242fd49e906522a4
Esther Ng <esther.hc.ng@gmail.com>
2016-10-06 13:08:24 -0700

    Got new link and view working but need to finish create/build action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,21 @@
 class ProductsController < ApplicationController
   def new
-    id = params[:vendor_id]
-    @vendor = Vendor.find(id)
-    @vendor_product = @vendor.products.build
+    vendor_id = params[:vendor_id]
+    vendor = Vendor.find(vendor_id)
+    @vendor_product = vendor.products.build
   end
 
+  def create
+    vendor_id = params[:vendor_id]
+    vendor = Vendor.find(vendor_id)
+    @vendor_product = vendor.products.create(vendor_product_params)
+
+    redirect_to vendor_path(vendor_id)
+  end
 
   private
 
-  # def  vendor_product_params
-  #   params.require(:product).permit(:name)
-  # end
+  def  vendor_product_params
+    params.require(:product).permit(:name, :vendor_id)
+  end
 end
